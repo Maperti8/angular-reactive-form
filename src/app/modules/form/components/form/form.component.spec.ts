2a2a2f8de29ff4feec8d5c6e06e76ca1e05342ae
Martin-Pekny <martin.pekny7@gmail.com>
2023-11-13 22:46:15 +0100

    test: added 4 unit tests
    form create, required fields, max length check, successful submit

--- src/app/modules/form/components/form/form.component.spec.ts
+++ src/app/modules/form/components/form/form.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MaterialModule } from '../../../../modules/material/material-module';
 import { FormComponent } from './form.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -9,7 +10,7 @@ describe('FormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MaterialModule, NoopAnimationsModule],
+      imports: [MaterialModule, NoopAnimationsModule, ReactiveFormsModule], 
       declarations: [FormComponent],
     }).compileComponents();
 
@@ -18,7 +19,50 @@ describe('FormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create FormComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create a form with invalid initial state', () => {
+    expect(component.profileForm.valid).toBe(false);
+  });
+
+  it('should be invalid when required fields are not filled in', () => {
+    component.profileForm.setValue({
+      firstName: '',
+      lastName: '',
+      age: null,
+      prevAttended: false,
+      sport: '',
+      comment: ''
+    });
+    expect(component.profileForm.valid).toBe(false);
+  });  
+
+  it('should be invalid when input values exceed maximum length', () => {
+    component.profileForm.setValue({
+      firstName: 'Lorem ipsum dolor sit amet, consectetur',
+      lastName: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      age: 30,
+      prevAttended: false,
+      sport: 'Football',
+      comment: 'This is a comment that exceeds the maximum length of 4000 characters. '.repeat(100)
+    });
+    expect(component.profileForm.valid).toBe(false);
+  });
+
+  it('should submit the form when all fields are valid', () => {
+    spyOn(component, 'openModal');
+    component.profileForm.setValue({
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 25,
+      prevAttended: false,
+      sport: 'Football',
+      comment: ''
+    });
+    component.onSubmit();
+    expect(component.openModal).toHaveBeenCalledWith('Form submitted successfully');
+  });
+  
 });
